7aaf0695b3e4e958dfc53e2ac379f4782b57aef1
Jonatan Sombié <jonatan.sombie@pactilis.com>
2020-03-31 18:04:39 +0200

    setup build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,13 @@
 module.exports = {
   stories: ['../stories/**/*.stories.{js,mdx}'],
+
+  addons: [
+    'storybook-prebuilt/addon-actions/register.js',
+    'storybook-prebuilt/addon-knobs/register.js',
+    'storybook-prebuilt/addon-a11y/register.js',
+    'storybook-prebuilt/addon-docs/register.js',
+  ],
+
   esDevServer: {
     // custom es-dev-server options
   },
